58d4609ce31c133a4f350027de3e7d950a3c4faa
TrojanFish <99603885+TrojanFish@users.noreply.github.com>
2026-03-12 10:26:05 +0800

    Fix CI: run black on all files, update .gitignore, and remove temporary files

--- run_page/nike_sync.py
+++ run_page/nike_sync.py
@@ -308,14 +308,14 @@ def parse_no_gpx_data(activity):
     max_heartrate = None
     average_cadence = None
     max_speed = None
-    
+
     summary_info = activity.get("summaries", [])
     distance = 0
 
     for s in summary_info:
         metric_type = s.get("metric")
         value = s.get("value", 0)
-        
+
         if metric_type == "distance":
             distance = value * 1000
         elif metric_type == "heart_rate":
@@ -325,7 +325,7 @@ def parse_no_gpx_data(activity):
         elif metric_type == "cadence":
             average_cadence = value
         elif metric_type == "speed_max":
-            max_speed = value # units depend on the device, usually km/h or m/s
+            max_speed = value  # units depend on the device, usually km/h or m/s
 
     # maybe training that no distance
     if not distance:
@@ -361,7 +361,11 @@ def parse_no_gpx_data(activity):
         "distance": distance,
         "moving_time": moving_time,
         "elapsed_time": elapsed_time,
-        "average_speed": distance / int(activity["active_duration_ms"] / 1000) if activity.get("active_duration_ms") else 0,
+        "average_speed": (
+            distance / int(activity["active_duration_ms"] / 1000)
+            if activity.get("active_duration_ms")
+            else 0
+        ),
         "elevation_gain": 0,
         "location_country": "",
     }
