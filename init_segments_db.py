9c5a516f45aa9fa4e669c051f96b2932add4e909
TrojanFish <99603885+TrojanFish@users.noreply.github.com>
2026-03-16 21:16:58 +0800

    Cleanup temporary debug scripts and garbage files

--- init_segments_db.py
+++
@@ -1,6 +0,0 @@
-import sys
-import os
-sys.path.append(os.getcwd())
-from run_page.db import init_db
-init_db("run_page/data.db")
-print("Database initialized with segments and segment_efforts tables.")
